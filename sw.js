4353c6def1c861a086a1ca1956e766073cca9f6b
Agustina <91155693+AGCO92@users.noreply.github.com>
2026-08-19 07:49:28 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = "gym-tracker-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request)
+        .then((response) => {
+          if (event.request.method === "GET" && response.ok) {
+            const clone = response.clone();
+            caches.open(CACHE).then((c) => c.put(event.request, clone));
+          }
+          return response;
+        })
+        .catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
